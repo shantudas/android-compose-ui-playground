bbcf51e51a0d5182f65fb8f84fd14cdad3bb4686
shantudash <shantudasbd@yahoo.com>
2020-12-10 12:56:58 +0600

    first commit

--- app/src/main/java/com/example/compose_playground/MainActivity.kt
+++ app/src/main/java/com/example/compose_playground/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.compose_playground
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.unit.dp
+import androidx.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContent {
+            MyComposeView()
+        }
+    }
+
+
+    @Composable
+    private fun MyComposeView() {
+        Column(
+            modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Hey, compose ui")
+            Spacer(modifier = Modifier.padding(top = 10.dp))
+            Button(onClick = {}) {
+                Text(text = "BUTTON")
+            }
+        }
     }
 }
