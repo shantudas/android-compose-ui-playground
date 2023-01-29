826b85ca8327816225eadae069162056063a5e51
Shantu Chandra Das <shantudasbd@yahoo.com>
2023-01-29 23:31:16 +0600

    starting point implemented

--- app/src/main/java/com/example/compose_playground/MainScreen.kt
+++ app/src/main/java/com/example/compose_playground/MainScreen.kt
@@ -7,12 +7,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
+@Preview()
 fun MainScreen() {
     Column(
         modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.SpaceEvenly,
+        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(text = "Hello! from compose screen")
